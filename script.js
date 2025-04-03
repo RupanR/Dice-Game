bc8a0bd2accd84c330c788c2d9eb271b1f720264
RupanR <rupanmentor@gmail.com>
2025-04-03 19:16:53 +0530

    base script completed

---
+++ script.js
@@ -0,0 +1,8 @@
+//! Getting the elements from html
+
+const dice = document.getElementById("dice");
+const playerMove = document.getElementById("player-move");
+const player1Score = document.getElementById("player1-score");
+const player2Score = document.getElementById("player2-score");
+const player1Button = document.getElementById("player1-button");
+const player2Button = document.getElementById("player2-button");
